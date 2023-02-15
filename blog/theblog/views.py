32aa29a87639572a1e0f7ad7dea893c60db1c20a
Valetra <valetra100102@gmail.com>
2023-02-15 20:25:36 +0300

    added delete abillity

--- blog/theblog/views.py
+++ blog/theblog/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DeleteView, CreateView, UpdateView
+#from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from django.views.generic import (
+    ListView, 
+    DeleteView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 class ArticleDetailView(DeleteView):
     model = Post
@@ -16,4 +25,13 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
-    #fields = '__all__'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
