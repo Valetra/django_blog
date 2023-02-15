ec6f0664be7d4cb1ac7b43202b98f80757737675
Valetra <valetra100102@gmail.com>
2023-02-15 19:27:41 +0300

    added bootstrap styles to add_post.html

--- blog/theblog/urls.py
+++ blog/theblog/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .views import (
     HomeView, 
     ArticleDetailView,
+    AddPostView,
     )
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('artile/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post'),
 ]
--- blog/theblog/views.py
+++ blog/theblog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView
 
 from .models import Post
+from .forms import PostForm
 
 class HomeView(ListView):
     model = Post
@@ -10,3 +11,9 @@ class HomeView(ListView):
 class ArticleDetailView(DeleteView):
     model = Post
     template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    #fields = '__all__'
