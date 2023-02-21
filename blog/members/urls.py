2f79b977cb88491807af715a88fffc24724c5ebd
Valetra <valetra100102@gmail.com>
2023-02-21 14:36:12 +0300

    added edit_profile.html

--- blog/members/urls.py
+++ blog/members/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegisterView, UserLoginView
+from .views import UserRegisterView, UserLoginView, UserEditView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('my_login/', UserLoginView.as_view(), name='my_login'),
 ]
--- blog/members/views.py
+++ blog/members/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserChangeForm
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 
@@ -16,6 +16,18 @@ class UserRegisterView(generic.CreateView):
 
     def get_context_data(self, *args, **kwargs):
         return get_category_menu_context(self, UserRegisterView, *args, **kwargs)
+
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
+
+    def get_context_data(self, *args, **kwargs):
+        return get_category_menu_context(self, UserEditView, *args, **kwargs)
+  
 
 class UserLoginView(LoginView):
     form_class = AuthenticationForm
